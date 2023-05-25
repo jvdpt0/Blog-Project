61ea0e91c30cc1d07b850ac69c3797b961bdac6a
jvdpt0 <jvdpt0@gmail.com>
2023-05-25 18:01:35 -0300

    Bootstrap and new pages added

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from flask import Flask, render_template
 import requests
 
-app = Flask(__name__)
 
-blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
-all_posts = requests.get(blog_url).json()
+all_posts = requests.get('https://api.npoint.io/e804a12b698002b4dc64').json()
+app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    return render_template("index.html", posts=all_posts)
+def get_home_page():
+    return render_template('index.html', posts = all_posts)
 
 
 @app.route('/post/<int:id>')
 def get_post(id):
-    return render_template("post.html", id=id , posts=all_posts)
+    return render_template('post.html', id=id, posts = all_posts)
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
 
 
-if __name__ == "__main__":
+@app.route('/contact')
+def contact_page():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
     app.run(debug=True)
